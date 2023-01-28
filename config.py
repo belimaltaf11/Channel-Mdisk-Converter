640dd96f9cc1ca2a83283881f76f2a0890fcbd95
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2023-01-28 17:48:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,12 +12,12 @@ def is_enabled(value, default):
 API_ID = int(os.environ.get("API_ID", "14438754"))
 API_HASH = os.environ.get("API_HASH", "baeb7d71bd10d8b5d8a97fce7ad9707d")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5817794965:AAHSpZYSnCaNEcWO4KZdshBVJoeQ_FQ0_q8")
-DROPLINK_API = os.environ.get("DROPLINK_API")
+DROPLINK_API = os.environ.get("DROPLINK_API", "2c364f0262cd8c7d5848697aa0dbe1d41e5e7377")
 MDISK_API = os.environ.get("MDISK_API", "Vj3WrvQ78OVuOpk32lp4")
 ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "MdiskConvertorBotpro")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://cccc:cccc@cluster0.7xrqyz3.mongodb.net/?retryWrites=true&w=majority")
-WEBSITE = os.environ.get('WEBSITE')
+WEBSITE = os.environ.get('WEBSITE', 'https://droplink.coi')
 
 #  Optionnal variables
 INCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("INCLUDE_DOMAIN").split(",")) if os.environ.get("INCLUDE_DOMAIN") else []
