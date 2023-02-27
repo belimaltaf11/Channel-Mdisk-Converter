d65a992de61749f87bc40059a4c5deef06fc820d
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2023-02-27 17:06:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,21 +15,6 @@ class Bot(Client):
             plugins={"root": "plugins"},
             sleep_threshold=15,
         )
-
-    async def start(self):
-        await super().start()
-        me = await self.get_me()
-        self.mention = me.mention
-        self.username = me.username 
-        self.force_channel = FORCE_SUB
-        if FORCE_SUB:
-            try:
-                link = await self.export_chat_invite_link(FORCE_SUB)                  
-                self.invitelink = link
-            except Exception as e:
-                print(e)
-                print("Make Sure Bot admin in force sub channel")             
-                self.force_channel = None
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"       
